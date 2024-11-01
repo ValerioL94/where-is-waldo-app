61b53fffb926b613eb821a2ab9c80e7d978ad00c
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-11-01 19:17:54 +0100

    add header component

---
+++ frontend/src/components/HeaderComponent.tsx
@@ -0,0 +1,11 @@
+const HeaderComponent = () => {
+  return (
+    <header className='flex items-center justify-center p-8 h-10 border-b-2 border-b-indigo-900 bg-indigo-400'>
+      <h1 className='text-2xl font-bold'>
+        Find the 3 characters in the shortest time!
+      </h1>
+    </header>
+  );
+};
+
+export default HeaderComponent;
