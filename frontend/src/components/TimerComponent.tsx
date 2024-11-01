29522c7b89ef8ac9000229b0ab12d7779109802b
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-11-01 19:18:13 +0100

    add timer component

---
+++ frontend/src/components/TimerComponent.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+
+const Timer = () => {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(true);
+  const seconds = Math.floor((time % 6000) / 100);
+  const milliseconds = time % 100;
+  useEffect(() => {
+    let intervalId: number;
+    if (isRunning && time === 5999) {
+      setIsRunning(false);
+    }
+    if (isRunning) {
+      intervalId = setInterval(() => setTime(time + 1), 10);
+    }
+    return () => clearInterval(intervalId);
+  }, [isRunning, time]);
+  return (
+    <div className='text-xl border-2 p-4 rounded-md border-indigo-900'>
+      <strong>
+        Time: {seconds.toString().padStart(2, '0')}:
+        {milliseconds.toString().padStart(2, '0')}
+      </strong>
+    </div>
+  );
+};
+
+export default Timer;
