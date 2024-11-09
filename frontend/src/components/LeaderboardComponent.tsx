9fc525035cb4214385e827591e5fb0f4827ad135
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-11-09 11:50:40 +0100

    add leaderboard component

---
+++ frontend/src/components/LeaderboardComponent.tsx
@@ -0,0 +1,45 @@
+import { gameMaps } from '../db';
+import { Stage } from './types';
+
+const LeaderboardComponent = ({
+  setStage,
+}: {
+  setStage: React.Dispatch<React.SetStateAction<Stage>>;
+}) => {
+  return (
+    <div className='flex flex-col items-center justify-center m-4 gap-4'>
+      <h1 className='mb-2 font-bold text-xl'>LEADERBOARD </h1>
+      <div className='flex gap-4 flex-wrap justify-center'>
+        {gameMaps.map((gameMap) => (
+          <div className='border-indigo-900 border-2 min-w-60 rounded-lg'>
+            <h1 className='border-indigo-900 border-b-2 p-2 text-lg font-bold text-indigo-700 text-center'>
+              {gameMap.name} map
+            </h1>
+            {!gameMap.scoreboard.length && (
+              <p className='text-center p-4'>No scores yet.</p>
+            )}
+            {gameMap.scoreboard && (
+              <ol className='list-decimal list-inside'>
+                {gameMap.scoreboard.map((player) => (
+                  <li className='p-2 border-b-2 m-2'>
+                    {player.name} - {player.score}
+                  </li>
+                ))}
+              </ol>
+            )}
+          </div>
+        ))}
+      </div>
+      <button
+        onClick={() => {
+          setStage(Stage.mapSelection);
+        }}
+        className='mt-8 border-2 p-2 rounded-md border-indigo-900 bg-indigo-400 font-bold text-black hover:bg-indigo-900 hover:border-indigo-900 hover:text-white focus:bg-indigo-900 focus:border-indigo-900 focus:text-white outline-none transition-transform hover:scale-110 focus:scale-110'
+      >
+        Go to map selection
+      </button>
+    </div>
+  );
+};
+
+export default LeaderboardComponent;
