b9f66644f2d5cc9380a2e010e0956c3135ad0aa8
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-11-09 19:22:46 +0100

    update header/footer styles

--- frontend/src/components/HeaderComponent.tsx
+++ frontend/src/components/HeaderComponent.tsx
@@ -1,6 +1,6 @@
 const HeaderComponent = () => {
   return (
-    <header className='flex items-center justify-center p-8 border-b-2 border-b-indigo-900 bg-indigo-400'>
+    <header className='flex items-center justify-center border-b-2 border-b-indigo-900 bg-indigo-400'>
       <h1 className='text-2xl font-bold'>Where's Waldo?</h1>
     </header>
   );
