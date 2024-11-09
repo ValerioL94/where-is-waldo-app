2c4bceaddf142f0237a0f84b5367c14aabe9b176
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-11-09 11:51:00 +0100

    add modal prompt

---
+++ frontend/src/components/ModalPromptComponent.tsx
@@ -0,0 +1,58 @@
+import { Character } from './types';
+
+const ModalPromptComponent = ({
+  characters,
+  setCharacters,
+  showModal,
+  x,
+  y,
+}: {
+  characters: Character[];
+  setCharacters: React.Dispatch<React.SetStateAction<Character[]>>;
+  showModal: React.Dispatch<React.SetStateAction<boolean>>;
+  x: number;
+  y: number;
+}) => {
+  function handleConfirm(id: number) {
+    showModal(false);
+    setCharacters(
+      characters.map((character) => {
+        if (character.id === id) {
+          return { ...character, found: true };
+        } else {
+          return character;
+        }
+      })
+    );
+  }
+  return (
+    <div
+      className='flex flex-col gap-2 items-center justify-center absolute min-w-28 h-40  bg-slate-400 border-indigo-900 border-2 rounded-md p-1 pt-4'
+      style={{
+        left: x > window.innerWidth / 2 ? x - 150 : x + 25,
+        top: y - 25,
+      }}
+    >
+      <button
+        className='absolute -top-2 right-2 bg-white rounded-full -mr-4 p-1 leading-[0.6] border-2 border-indigo-900 font-bold'
+        onClick={() => showModal(false)}
+      >
+        x
+      </button>
+      {characters.map(
+        (character) =>
+          !character.found && (
+            <button
+              key={character.id}
+              className='w-full border-slate-300 border-2 rounded-md hover:bg-slate-300 p-1 font-bold'
+              onClick={() => handleConfirm(character.id)}
+            >
+              {character.name}
+            </button>
+          )
+      )}
+    </div>
+  );
+};
+
+export default ModalPromptComponent;
