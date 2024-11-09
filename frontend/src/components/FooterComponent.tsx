7d7a7b7f392bdc603421951995c799a76fa741e0
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-11-09 11:49:45 +0100

    add conditional rendering

--- frontend/src/components/FooterComponent.tsx
+++ frontend/src/components/FooterComponent.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
-
 const FooterComponent = () => {
   return (
-    <footer className='w-full flex items-center justify-center p-8 h-10 gap-2 border-t-2 border-t-indigo-900 bg-indigo-400'>
+    <footer className='w-full flex items-center justify-center p-8 gap-2 border-t-2 border-t-indigo-900 bg-indigo-400'>
       <p className='font-bold'>Copyright © 2024 ValerioL94</p>
       <a
         className='hover:animate-pulse focus:animate-pulse'
--- frontend/src/components/MainComponent.tsx
+++ frontend/src/components/MainComponent.tsx
@@ -1,77 +1,25 @@
 import { useState } from 'react';
-import Timer from './TimerComponent';
-import ModalPrompt from './ModalPrompt';
+import { GameMap, Stage } from './types';
+import MapSelectionComponent from './MapSelectionComponent';
+import LeaderboardComponent from './LeaderboardComponent';
+import GameComponent from './GameComponent';
 
 const MainComponent = () => {
-  const [isFound, setIsFound] = useState(false);
-  const [clickPosition, setClickPosition] = useState({ x: 0, y: 0 });
-  const [modalPrompt, setModalPrompt] = useState(false);
-  function checkColor() {
-    if (isFound) {
-      return 'max-h-36 max-w-28 border-4 rounded-md border-green-500';
-    } else return 'max-h-36 max-w-28 border-4 rounded-md border-red-500';
-  }
-  function getCoords(e) {
-    const rect = e.currentTarget.getBoundingClientRect();
-    const coordX = e.clientX - rect.left;
-    const coordY = e.clientY - rect.top;
-    const newClick = { x: coordX, y: coordY };
-    setClickPosition(newClick);
-    setModalPrompt(true);
-  }
+  const [stage, setStage] = useState<Stage>(Stage.mapSelection);
+  const [gameMap, setGameMap] = useState<GameMap | undefined>(undefined);
+
   return (
-    <div className='flex flex-col lg:flex-row bg-gray-100'>
-      <div className='lg:w-3/4 relative'>
-        <img
-          className='cursor-crosshair'
-          src='/assets/images/alien-life-waldo.jpg'
-          alt='alien life'
-          onClick={getCoords}
-        />
-        {modalPrompt && (
-          <ModalPrompt
-            showModal={setModalPrompt}
-            x={clickPosition.x}
-            y={clickPosition.y}
-          />
-        )}
-      </div>
-      <div className='lg:fixed lg:left-3/4 pl-10'>
-        <div className='flex flex-col md:flex-row lg:flex-col items-center w-full py-4 gap-4'>
-          <Timer />
-          <div className='text-center'>
-            <span>
-              <strong>Robo-dog</strong>
-            </span>
-            <img
-              className={checkColor()}
-              src='/assets/icons/robo-dog.png'
-              alt='robot dog'
-            />
-          </div>
-          <div className='text-center'>
-            <span>
-              <strong>Alien dad</strong>
-            </span>
-            <img
-              className={checkColor()}
-              src='/assets/icons/alien-dad.png'
-              alt='alien dad'
-            />
-          </div>
-          <div className='text-center'>
-            <span>
-              <strong>Astronaut</strong>
-            </span>
-            <img
-              className={checkColor()}
-              src='/assets/icons/astronaut.png'
-              alt='astronaut'
-            />
-          </div>
-        </div>
-      </div>
-    </div>
+    <main className='bg-indigo-50'>
+      {stage === Stage.mapSelection && (
+        <MapSelectionComponent setGameMap={setGameMap} setStage={setStage} />
+      )}
+      {stage === Stage.gamePhase && (
+        <GameComponent gameMap={gameMap!} setStage={setStage} />
+      )}
+      {stage === Stage.leaderboard && (
+        <LeaderboardComponent setStage={setStage} />
+      )}
+    </main>
   );
 };
 
