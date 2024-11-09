8eacc077896f8dc844fda5192876a4869499619c
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-11-09 19:22:24 +0100

    update test db

--- frontend/src/db.ts
+++ frontend/src/db.ts
@@ -3,46 +3,59 @@ import { GameMap, Player } from './components/types';
 export const scoreboard1: Player[] = [
   { name: 'John', score: '05:20' },
   { name: 'Mary', score: '17:20' },
+  { name: '', score: '' },
+  { name: '', score: '' },
+  { name: '', score: '' },
 ];
-export const scoreboard2: Player[] = [];
+export const scoreboard2: Player[] = [
+  { name: '', score: '' },
+  { name: '', score: '' },
+  { name: '', score: '' },
+  { name: '', score: '' },
+  { name: '', score: '' },
+];
 export const scoreboard3: Player[] = [
   { name: 'CoolGuy', score: '09:22' },
   { name: 'Princess', score: '11:01' },
+  { name: '', score: '' },
+  { name: '', score: '' },
+  { name: '', score: '' },
 ];
 const map1: GameMap = {
   id: 1,
-  name: 'Alien-life',
+  name: 'Dangerous Sea',
   image: {
-    src: '/assets/images/alien-life-waldo.jpg',
-    alt: 'alien life map',
+    src: '/assets/images/wheres-waldo-dangerous-sea.jpg',
+    alt: 'dangerous sea map',
   },
+  difficulty: 'easy',
   characters: [
     {
       id: 1,
-      name: 'Robo-dog',
+      name: 'Waldo',
       icon: {
-        src: '/assets/icons/robo-dog.png',
-        alt: 'robot dog',
+        src: '/assets/images/waldo.jpg',
+        alt: 'waldo',
       },
       found: false,
       position: { x: 0, y: 0 },
     },
     {
       id: 2,
-      name: 'Alien dad',
+      name: 'Wilma',
       icon: {
-        src: '/assets/icons/alien-dad.png',
-        alt: 'alien dad',
+        src: '/assets/images/wilma.jpeg',
+        alt: 'wilma',
       },
       found: false,
       position: { x: 100, y: 100 },
     },
     {
       id: 3,
-      name: 'Astronaut',
+      name: 'Wizard',
       icon: {
-        src: '/assets/icons/astronaut.png',
-        alt: 'astronaut',
+        src: '/assets/images/wizard.jpeg',
+        alt: 'wizard',
       },
       found: false,
       position: { x: 200, y: 200 },
@@ -53,38 +66,39 @@ const map1: GameMap = {
 
 const map2: GameMap = {
   id: 2,
-  name: 'Hollywood-life',
+  name: 'Hollywood',
   image: {
-    src: '/assets/images/hollywood-life-waldo.jpg',
-    alt: 'hollywood life map',
+    src: '/assets/images/wheres-waldo-hollywood.jpg',
+    alt: 'hollywood map',
   },
+  difficulty: 'medium',
   characters: [
     {
       id: 1,
-      name: '',
+      name: 'Wenda',
       icon: {
-        src: '',
-        alt: '',
+        src: '/assets/images/wenda.jpg',
+        alt: 'wenda',
       },
       found: false,
       position: { x: 0, y: 0 },
     },
     {
-      id: 1,
-      name: '',
+      id: 2,
+      name: 'Waldo',
       icon: {
-        src: '',
-        alt: '',
+        src: '/assets/images/waldo.jpg',
+        alt: 'waldo',
       },
       found: false,
       position: { x: 0, y: 0 },
     },
     {
-      id: 1,
-      name: '',
+      id: 3,
+      name: 'Odlaw',
       icon: {
-        src: '',
-        alt: '',
+        src: '/assets/images/odlaw.jpg',
+        alt: 'odlaw',
       },
       found: false,
       position: { x: 0, y: 0 },
@@ -94,38 +108,39 @@ const map2: GameMap = {
 };
 const map3: GameMap = {
   id: 3,
-  name: 'Medieval-life',
+  name: 'Amusement Park',
   image: {
-    src: '/assets/images/medieval-life-waldo.jpg',
-    alt: 'medieval life map',
+    src: '/assets/images/wheres-waldo-amusement-park.jpg',
+    alt: 'amusement park map',
   },
+  difficulty: 'hard',
   characters: [
     {
       id: 1,
-      name: '',
+      name: 'Odlaw',
       icon: {
-        src: '',
-        alt: '',
+        src: '/assets/images/odlaw.jpg',
+        alt: 'odlaw',
       },
       found: false,
       position: { x: 0, y: 0 },
     },
     {
-      id: 1,
-      name: '',
+      id: 2,
+      name: 'Waldo',
       icon: {
-        src: '',
-        alt: '',
+        src: '/assets/images/waldo.jpg',
+        alt: 'waldo',
       },
       found: false,
       position: { x: 0, y: 0 },
     },
     {
-      id: 1,
-      name: '',
+      id: 3,
+      name: 'Wizard',
       icon: {
-        src: '',
-        alt: '',
+        src: '/assets/images/wizard.jpeg',
+        alt: 'wizard',
       },
       found: false,
       position: { x: 0, y: 0 },
