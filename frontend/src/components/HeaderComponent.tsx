d8ef80c2db7ee6d8d1d4b9a1ef0b253dbdecdc2e
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-11-09 11:51:26 +0100

    update header component

--- frontend/src/components/HeaderComponent.tsx
+++ frontend/src/components/HeaderComponent.tsx
@@ -1,9 +1,7 @@
 const HeaderComponent = () => {
   return (
-    <header className='flex items-center justify-center p-8 h-10 border-b-2 border-b-indigo-900 bg-indigo-400'>
-      <h1 className='text-2xl font-bold'>
-        Find the 3 characters in the shortest time!
-      </h1>
+    <header className='flex items-center justify-center p-8 border-b-2 border-b-indigo-900 bg-indigo-400'>
+      <h1 className='text-2xl font-bold'>Where's Waldo?</h1>
     </header>
   );
 };
